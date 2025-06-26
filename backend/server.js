f592dc89f6d4f8a88cd0161beb54613c21074335
Jeet115 <jeetmistry115@gmail.com>
2025-06-26 20:16:35 +0530

    added vercel link to cors

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173' || 'https://eraya-ratna.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
